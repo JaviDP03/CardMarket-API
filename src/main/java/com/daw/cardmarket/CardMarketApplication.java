58953f56acb1098981aac774a7682adc2c4815b6
Javier Duarte <javiduarte34@gmail.com>
2025-04-18 17:01:13 +0200

    Proyecto iniciado

---
+++ src/main/java/com/daw/cardmarket/CardMarketApplication.java
@@ -0,0 +1,13 @@
+package com.daw.cardmarket;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CardMarketApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CardMarketApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/daw/cardmarket/CardMarketApplicationTests.java
@@ -0,0 +1,13 @@
+package com.daw.cardmarket;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CardMarketApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
